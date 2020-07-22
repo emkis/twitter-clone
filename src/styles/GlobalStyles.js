ed95b61f416feb7b36c50d765541edf35cddda46
Nicolas Emkis <nicolasemkis@gmail.com>
2020-07-21 23:59:30 -0300

    base structure created

---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html, body, #root {
+    max-height: 100vh;
+    max-width: 100vw;
+    width: 100%;
+    height: 100%;
+  }
+
+  *, button, input {
+    border: 0;
+    background: none;
+    font-family: --apple-system, system-ui, BlinkMacSystemFont, 'Segoe UI', Roboto, Ubuntu,'Helvetica Neue', sans-serif;
+  }
+`;
