2c439c55916bab166a4fe45158c9c4cf96091e10
Nicolas Emkis <nicolasemkis@gmail.com>
2020-07-27 23:52:09 -0300

    add component

---
+++ src/components/News/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import { Container } from './styles'
+
+function News() {
+  return (
+    <Container>
+      <span>Covid-19 · This afternoon</span>
+      <strong>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae.
+      </strong>
+    </Container>
+  )
+}
+
+export default News
---
+++ src/components/News/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  font-size: 14px;
+
+  > span {
+    display: block;
+    margin-bottom: 3px;
+
+    color: var(--gray);
+  }
+`
